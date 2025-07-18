5ccce3ce1b8839a6ddf44629d65b07c16a622751
earth nut dev <earthnut.dev@outlook.com>
2025-07-18 21:21:52 +0800

    submit :  7/18/2025, 9:21:52 PM

--- dog.ts
+++ dog.ts
@@ -1,6 +1,6 @@
-import { DevLog, Dog } from '@qqi/log';
+import { Dog } from '@qqi/log';
 /**  开发环境执行，该方法在 env === 'production' 时通过 babel 和 webpack 过滤移除  */
-export const dog: DevLog = new Dog({
+export const dog = new Dog({
   name: 'earthnut',
   type: false,
 });
